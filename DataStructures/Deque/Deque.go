e0e4f34402be137001f4d075b59ea564dda6da90
Александр Подольский <a.a.podolskiy@gmail.com>
2022-12-10 23:01:23 +0300

    datastructures new (#10)

---
+++ DataStructures/Deque/Deque.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+)
+
+//////////////////////////////////////////////
+////////// MyDeque implementation //////////
+
+type I interface {
+	PeekLeft() int
+	PeekRight() int
+	PopLeft() int
+	PopRight() int
+	InsertLeft(int)
+	InsertRight(int)
+	Empty() bool
+}
+
+type MyCircularDeque struct {
+}
+
+func Constructor(k int) MyCircularDeque {
+
+}
+
+func (this *MyCircularDeque) InsertFront(value int) bool {
+
+}
+
+func (this *MyCircularDeque) InsertLast(value int) bool {
+
+}
+
+func (this *MyCircularDeque) DeleteFront() bool {
+
+}
+
+func (this *MyCircularDeque) DeleteLast() bool {
+
+}
+
+func (this *MyCircularDeque) GetFront() int {
+
+}
+
+func (this *MyCircularDeque) GetRear() int {
+
+}
+
+func (this *MyCircularDeque) IsEmpty() bool {
+
+}
+
+func (this *MyCircularDeque) IsFull() bool {
+
+}
+
+////////// MyMaxHeap implementation //////////
+//////////////////////////////////////////////
+
+func main() {
+	// todo
+}
